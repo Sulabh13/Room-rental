05950141e802332ea52442a1d22c86973d642f92
NikhilWaghade <nikhilwaghade031003@gmail.com>
2026-03-23 23:19:52 +0530

    Update frontend Deploy url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,11 +1,15 @@
 import axios from "axios";
 
+const baseURL =
+    import.meta.env.MODE === "development"
+        ? "http://localhost:5000/api"
+        : "https://room-rental-backend-0a5u.onrender.com/api";
+
 const API = axios.create({
-    baseURL: "http://localhost:5000/api"
+    baseURL,
 });
 
 API.interceptors.request.use((req) => {
-
     const token = localStorage.getItem("token");
 
     if (token) {
